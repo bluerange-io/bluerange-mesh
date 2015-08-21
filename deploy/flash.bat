4e79d7b81790cd440818f0e8b5a47276e0b4c7ed
Marius Heil <mariusheil@gmail.com>
2015-08-21 15:07:43 +0200

    Rewrote deployment script to work with a list of segger ids which makes it easier to handle new dongles.

--- deploy/flash.bat
+++ deploy/flash.bat
@@ -1,31 +1,53 @@
+@echo off
+
+echo Arguments:
+echo     1: softdevice or fruitymesh or reset (what to flash)
+echo     2: name of textfile which contains newline seperated segger ids
+echo     3: Debug or Release (The folder from which to take the application hex)
+
+REM setting some defaults
+
+SET TYPE=fruitymesh
+SET IDS=dongle_segger_ids.txt
+SET BUILD=Debug
+
+IF NOT [%1] == [] SET TYPE=%1
+IF NOT [%2] == [] SET IDS=%2
+IF NOT [%3] == [] SET BUILD=%3
+
+SET EXEC_FOLDER=C:\nrf\projects\fruitymesh\deploy
 SET NRFJPROG_PATH=C:\nrf\tools\nrf51_tools\bin\nrfjprog.exe
+
+SET FRUITYMESH_HEX=C:\nrf\projects\fruitymesh\%BUILD%\FruityMesh.hex
 SET SOFTDEVICE_HEX=C:\nrf\softdevices\sd130_1.0.0-prod\s130_nrf51_1.0.0_softdevice.hex
 
-REM pass the folder name, either Debug or Release to this script file
-if("%1" == "") SET 1 Debug
+echo.
+echo.
 
-echo Parallel flashing on all dongles
+echo Parallel flashing on all dongles in %IDS% with %TYPE% and Build %BUILD%
 
-SET SEGGER0=680194045
-SET SEGGER1=680449458
-SET SEGGER2=680990847
-SET SEGGER3=680603635
-SET SEGGER4=680837880
-SET SEGGER5=680966072
-SET SEGGER6=680084304
-SET SEGGER7=680116493
-SET SEGGER8=680554309
+echo.
+echo.
 
-SET APPHEXFILE=..\%1\FruityMesh.hex
+echo IDs:
 
-start /min %NRFJPROG_PATH% -s %SEGGER0% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER1% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER2% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER3% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER4% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER5% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER6% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER7% --program %APPHEXFILE% -r
-start /min %NRFJPROG_PATH% -s %SEGGER8% --program %APPHEXFILE% -r
+cd %EXEC_FOLDER%
+for /f "tokens=* delims=" %%x in (%IDS%) do (
 
+	echo | set /p dummyName=%%x, 
+
+	if %TYPE% == fruitymesh (
+		start /min %NRFJPROG_PATH% -s %%x --program %FRUITYMESH_HEX% -r
+	)
+	if %TYPE% == softdevice (
+		start /min %NRFJPROG_PATH% -s %%x --erase --program %SOFTDEVICE_HEX%
+	)
+	if %TYPE% == reset (
+		start /min nrfjprog -s %%x -r
+	)
+
+)
+
+echo.
+echo.
 echo "Flashing finished"
